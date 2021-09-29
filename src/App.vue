b9f94b12dc24176e4f6e8184eaa628a19bc36729
Leonardo <leozikasantos73@gmail.com>
2021-09-29 12:42:52 -0300

    primeiro commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,21 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+    <AppTopBar />
+    <v-main>
+      <Inicio/>
+    </v-main>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Inicio from './components/Inicio.vue';
+import AppTopBar from './components/shared/AppTopBar.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Inicio,
+    AppTopBar
   }
-}
+};
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/shared/AppTopBar.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="app-top-bar">
+    <v-app-bar
+      app
+      color="green lighten-1"
+      dark
+    >
+      <v-app-bar-nav-icon @click="drawer = true"></v-app-bar-nav-icon>
+      <v-toolbar-title>Campeonato Brasileiro 2021 </v-toolbar-title>
+    </v-app-bar>
+
+    <v-navigation-drawer
+      v-model="drawer"
+      absolute
+      temporary
+    >
+      <v-list
+        nav
+        dense
+      >
+        <v-list-item-group
+          v-model="group"
+          active-class="green lighten-1"
+        >
+          <v-list-item>
+            <v-list-item-icon>
+              <v-icon>mdi-home</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>Home</v-list-item-title>
+          </v-list-item>
+
+          <v-list-item>
+            <v-list-item-icon>
+              <v-icon>mdi-soccer</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>Classificação</v-list-item-title>
+          </v-list-item>
+        </v-list-item-group>
+      </v-list>
+    </v-navigation-drawer>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'AppTopBar',
+    data: () => ({
+      drawer: false,
+      group: null
+  }),
+}
+</script>
+
+<style scoped>
+
+</style>
